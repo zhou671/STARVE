f665a058bb194ab9c18a8e29f5f7d208a1f0fa6c
Yue Wang <wangyue.pl@gmail.com>
2021-04-15 21:19:54 -0400

    'add'

--- hyperparams/loss_param.py
+++ hyperparams/loss_param.py
@@ -2,6 +2,9 @@ class LossParam:
     """
     Hyper-parameters for utils/losses.py
     """
-    content_weight = 1e-1
-    style_weight = 5e-6
+    content_weight = 1 # alpha
+    style_weight = 20 # beta
     tv_weight = 1e-1  # total variation loss weight
+    temporal_weight = 200  #lambda
+    blend_factor = 0.5 # delta
+    J = [1, 10, 20, 40] # long-term consistency chosen frame
