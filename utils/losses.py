51f9d6ed2f50996953e2b033efc5805599ec57ee
Yue Wang <wangyue.pl@gmail.com>
2021-04-15 21:19:30 -0400

    use index

--- utils/losses.py
+++ utils/losses.py
@@ -50,11 +50,15 @@ def init_multipass_img(img_sqe, frame_idx, direction):
     see equation (11) and (12)
     :param img_sqe: list of images, each img should be WxHxC
     :param frame_idx: the idx of the frame to be count loss for
-    :param direction: passing direction, either 1 or -1 
+    :param direction: passing direction, 1 if forward, -1 if backward
     :return: a WxHxC image
     """
     n = len(img_sqe)
-    prev_frame_idx = frame_idx + direction
+    if direction == 1:  # if forward, i-1 to i
+        prev_frame_idx = frame_idx - direction
+    elif direction == -1:  # if backward, i to i+1
+        prev_frame_idx = frame_idx
+        frame_ind += 1
     if prev_frame_idx < 0 or prev_frame_idx > n:
         return tf.identity(img_sqe[frame_idx])
     else:
@@ -79,10 +83,10 @@ def gram_matrix(input_tensor):
 def style_content_loss(outputs, style_targets, content_targets):
     """
     Loss = content_weight * content_loss + style_weight * style_loss
-    :param outputs:
-    :param style_targets:
-    :param content_targets:
-    :return:
+    :param outputs: a dictionary of style and content output
+    :param style_targets: extracted style features of the style image
+    :param content_targets: extracted content features of the style image
+    :return: combined style and content loss
     """
     style_outputs = outputs['style']
     content_outputs = outputs['content']
@@ -98,6 +102,39 @@ def style_content_loss(outputs, style_targets, content_targets):
 
     return loss
 
+def temporal_loss(generated_image, warped_images, per_pxl_weight):
+    """
+    see equation (7) for how this loss is defined
+    :param generated_image: 1XWxHx3 image, x in equation
+    :param warped_images: nXWxHx3 warped images.
+           omega(x) in equation
+    :param per_pxl_weight: 1XWxHx1 per_pxl_weight, c in equation
+    :return:
+        loss: weighted temporal loss
+    """
+    loss = generated_image - warped_images
+    loss = tf.math.multiply(loss, loss)
+    loss = tf.math.multiply(per_pxl_weight, loss)
+    loss = LossParam.temporal_weight * tf.reduce_mean(loss)
+
+    return loss
+
+def shortterm_loss(img_sqe, frame_idx, outputs, generated_image, style_targets, content_targets, warped_images):
+    '''
+    see equation (8)
+    :param img_sqe: list of images, each img should be WxHxC
+    :param frame_idx: the idx of the frame to be count loss for
+    :param outputs: a dictionary of style and content output
+    :param style_targets: extracted style features of the style image
+    :param content_targets: extracted content features of the style image
+    :return: combined style, content loss and temporal loss
+    '''
+    prev_frame_idx = frame_idx - 1
+    if prev_frame_idx < 0:
+        return style_content_loss(outputs, style_targets, content_targets)
+    per_pxl_weight = get_per_pxl_weight(frame_idx, prev_frame_idx)
+    loss = style_content_loss(outputs, style_targets, content_targets) + 
+           temporal_loss(generated_image[frame_idx], warped_images[prev_frame_idx]*(generated_image[prev_frame_idx]), per_pxl_weight)
 
 def tv_loss(generated_image):
     """
